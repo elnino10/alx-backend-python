7c94f85fb2e4f7d8cdf0a8d5494e785f55aaf6f5
Joe <joeegboka@gmail.com>
2024-05-02 19:40:01 +0100

    3-to_str.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - add module"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return a float"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return a string"""
+    return str1 + str2
